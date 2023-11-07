c0fb7b4d7b8c81d6c9086eef6721947a6bde7a76
Mohamed-Ahmed-Abd-Elkarim <09mohamed1922@gmail.com>
2023-11-07 12:58:34 +0200

    Template KFC

---
+++ main.js
@@ -0,0 +1,22 @@
+let hamburer = document.querySelector(".hamburger");
+
+hamburer.onclick = function () {
+    let nav = document.querySelector("nav");
+    nav.classList.toggle("open");
+};
+
+let span = document.querySelector(".up");
+window.onscroll = function () {
+    // console.log(this.scrollY);
+    if (this.scrollY >= 1000) {
+        span.classList.add("show");
+    } else {
+        span.classList.remove("show");
+    }
+};
+span.onclick = function () {
+    window.scrollTo ({
+        top:0,
+    });
+};
+
